3f0337ea99523c542383514df042a5b55190963c
Priyansh Garg <priyanshgarg30@gmail.com>
2024-08-07 18:08:49 +0530

    Refactor screenshot hook.

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -1,9 +1,13 @@
 const {AfterStep} = require('@cucumber/cucumber');
 
-AfterStep(async function(testStep) {
-  // take screenshot on failure after each step
-  if (testStep.result.status === 'FAILED' && this.browser) {
-    await this.browser.saveScreenshot('failure-screenshot.png');
+AfterStep(async function(testCase) {
+  // take screenshot on test case failure.
+  //
+  // follow this Discord thread for integrating the screenshots into your Cucumber report:
+  // https://discord.com/channels/618399631038218240/1253341054921609306/1255939897047515250
+  //
+  if (testCase.result.status === 'FAILED' && this.browser) {
+    await this.browser.saveScreenshot(`screenshots/${testCase.pickle.name}.png`);
   }
 });
 
